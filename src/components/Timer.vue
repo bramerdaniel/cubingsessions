485fc839ac5553820ae249a5c8115f08786876cc
Bramer <Daniel.Bramer@kuka.com>
2019-03-27 19:57:18 +0100

    initial commit of source code

---
+++ src/components/Timer.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="hello" @keyup.space="startTimer" @keydown.space="stopTimer">
+    <h1>Cubing sessions timer</h1>
+    <h1>{{ timeString }}</h1>
+    <button name="b">Timer</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Stopwatch } from "ts-stopwatch";
+import moment from 'moment';
+
+@Component
+export default class Timer extends Vue {
+  created() {
+    window.addEventListener("keyup", () => this.startTimer());
+    window.addEventListener("keydown", () => this.stopTimer());
+  }
+
+  get timeString() {
+    const d = new Date(this.time);
+    return moment(d).format('MM:SSS');
+  }
+
+  protected stopWatch: Stopwatch = new Stopwatch();
+  protected time: number = 0;
+  protected interval: number = 0;
+  protected timerIsRunning: boolean = false;
+
+  onKeyUp(): void {
+    if (!this.timerIsRunning) {
+      return
+    }
+
+    this.startTimer();
+  }
+
+  startTimer(): void {
+    if (this.timerIsRunning) return;
+
+    this.stopWatch.reset();
+    console.log("timer started");
+    this.stopWatch.start();
+    this.timerIsRunning = true;
+    this.interval = setInterval(this.updateTime, 100);
+  }
+
+  stopTimer(): void {
+    if (this.timerIsRunning) {
+      this.stopWatch.stop();
+      this.time = this.stopWatch.getTime();
+      console.log("timer stopped at " + this.time);
+      clearInterval(this.interval);
+      setTimeout(() => (this.timerIsRunning = false), 100);
+    }
+  }
+
+  updateTime(): void {
+    this.time = this.stopWatch.getTime();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Timer />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Timer from "@/components/Timer.vue"; // @ is an alias to /src
+
+
+@Component({
+  components: {
+    Timer
+  }
+})
+export default class Home extends Vue {}
+
+
+</script>
